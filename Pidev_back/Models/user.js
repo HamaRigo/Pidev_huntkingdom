bd1301019abdc6f3af013bf180db1bf02118c3c8
mohamed_bouallagui <mohamed.bouallagui@esprit.tn>
2022-06-30 14:43:23 +0100

    Add Crud User

--- Pidev_back/Models/user.js
+++ Pidev_back/Models/user.js
@@ -1,13 +1,19 @@
 var mongoose = require("mongoose");
 var schema = mongoose.Schema;
-var User = new schema({
+var user = new schema({
     email: { type: String, require: true , unique: true},
     username: { type: String, require: true},
     hash: { type: String, required: true },
-    adresse: { type: String},
     phone: { type: Number},
-    cin: { type: Number , require: true ,  unique: true},
     status: { type: Boolean, require: true}
 
 })
-module.exports= mongoose.model('user', User);
+user.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.hash;
+    }
+});
+module.exports= mongoose.model('users', user);
