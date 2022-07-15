80bc9a519c5f349755bfac9e46786089e03485cb
mohamed_bouallagui <mohamed.bouallagui@esprit.tn>
2022-07-15 16:32:41 +0100

    add middelware

--- Pidev_back/routes/profile.route.js
+++ Pidev_back/routes/profile.route.js
@@ -1,6 +1,6 @@
 const router=require('express').Router();
 const profileController=require('./../services/profile.controller')
-const middleware=require('../_helpers/middleware');
+const middleware=require('../helpers/middleware');
 router.get('/current-user',middleware.auth,profileController.current_user);
 router.post('/change-password',middleware.auth,profileController.change_password);
 router.put('/update-profile',middleware.auth,profileController.update_profile);
